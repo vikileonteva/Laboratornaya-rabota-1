4193ed9e2d3ac02ae09ed5dac426f7fbd30f168c
vikileonteva <vikibarsi21@gmail.com>
2025-02-11 00:24:56 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task_3.py
@@ -0,0 +1,51 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+
+        if not isinstance(pages, int):
+            raise TypeError("Страницы - только целые числа")
+        if pages <= 10:
+            raise ValueError("Страниц должно быть больше 10")
+        self.pages = pages
+
+    def __str__(self):
+        return super().__str__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+
+        if not isinstance(duration, float):
+            raise TypeError("Длительность книги должна быть числом с плавающей точкой")
+        if duration <= 10:
+            raise ValueError("Длительность книги более 10 минут")
+        self.duration = duration
+
+    def __str__(self):
+        return super().__str__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.duration})"
